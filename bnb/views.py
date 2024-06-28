dad382bd3461a2a83e75f901b06b73700a237931
Guillom8769 <8769@holbertonstudents.com>
2024-06-28 09:35:58 +0200

    point de recup gestion crud routes et templates et tous les modèles

--- bnb/views.py
+++ bnb/views.py
@@ -1,4 +1,3 @@
-# bnb/views.py
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import User, Place, Review, Amenity, Country, City
 from .forms import UserForm, PlaceForm, ReviewForm, AmenityForm, CountryForm, CityForm
@@ -54,7 +53,15 @@ def city_detail(request, pk):
     city = get_object_or_404(City, pk=pk)
     return render(request, 'bnb/city_detail.html', {'city': city})
 
-# Form views for User
+# User CRUD views
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'bnb/user_list.html', {'users': users})
+
+def user_detail(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    return render(request, 'bnb/user_detail.html', {'user': user})
+
 def user_create(request):
     if request.method == "POST":
         form = UserForm(request.POST)
@@ -83,7 +90,15 @@ def user_delete(request, pk):
         return redirect('user_list')
     return render(request, 'bnb/user_confirm_delete.html', {'user': user})
 
-# Form views for Place
+# Place CRUD views
+def place_list(request):
+    places = Place.objects.all()
+    return render(request, 'bnb/place_list.html', {'places': places})
+
+def place_detail(request, pk):
+    place = get_object_or_404(Place, pk=pk)
+    return render(request, 'bnb/place_detail.html', {'place': place})
+
 def place_create(request):
     if request.method == "POST":
         form = PlaceForm(request.POST)
@@ -112,7 +127,15 @@ def place_delete(request, pk):
         return redirect('place_list')
     return render(request, 'bnb/place_confirm_delete.html', {'place': place})
 
-# Form views for Review
+# Similar views for Review, Amenity, Country, City...
+def review_list(request):
+    reviews = Review.objects.all()
+    return render(request, 'bnb/review_list.html', {'reviews': reviews})
+
+def review_detail(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    return render(request, 'bnb/review_detail.html', {'review': review})
+
 def review_create(request):
     if request.method == "POST":
         form = ReviewForm(request.POST)
@@ -141,7 +164,14 @@ def review_delete(request, pk):
         return redirect('review_list')
     return render(request, 'bnb/review_confirm_delete.html', {'review': review})
 
-# Form views for Amenity
+def amenity_list(request):
+    amenities = Amenity.objects.all()
+    return render(request, 'bnb/amenity_list.html', {'amenities': amenities})
+
+def amenity_detail(request, pk):
+    amenity = get_object_or_404(Amenity, pk=pk)
+    return render(request, 'bnb/amenity_detail.html', {'amenity': amenity})
+
 def amenity_create(request):
     if request.method == "POST":
         form = AmenityForm(request.POST)
@@ -170,7 +200,14 @@ def amenity_delete(request, pk):
         return redirect('amenity_list')
     return render(request, 'bnb/amenity_confirm_delete.html', {'amenity': amenity})
 
-# Form views for Country
+def country_list(request):
+    countries = Country.objects.all()
+    return render(request, 'bnb/country_list.html', {'countries': countries})
+
+def country_detail(request, pk):
+    country = get_object_or_404(Country, pk=pk)
+    return render(request, 'bnb/country_detail.html', {'country': country})
+
 def country_create(request):
     if request.method == "POST":
         form = CountryForm(request.POST)
@@ -199,7 +236,14 @@ def country_delete(request, pk):
         return redirect('country_list')
     return render(request, 'bnb/country_confirm_delete.html', {'country': country})
 
-# Form views for City
+def city_list(request):
+    cities = City.objects.all()
+    return render(request, 'bnb/city_list.html', {'cities': cities})
+
+def city_detail(request, pk):
+    city = get_object_or_404(City, pk=pk)
+    return render(request, 'bnb/city_detail.html', {'city': city})
+
 def city_create(request):
     if request.method == "POST":
         form = CityForm(request.POST)
